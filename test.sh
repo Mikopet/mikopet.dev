db8c0634f6141d8ed683f70a85d04213e0009bc1
Peter Mikola <mikopet@gmail.com>
2020-12-27 12:40:05 +0100

    adding some format tests and format some files

--- test.sh
+++ test.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-git grep "href../" && exit 1
+INCLUDES="--include=\*.md --include=\*.html"
+EXCLUDES="--exclude-dir=_site --exclude-dir=.git --exclude=LICENSE.md"
 
+test_command () {
+  echo "Testing pattern: " $1
+  if [ "$2" == "noinclude" ] ; then
+    include=''
+  else
+    include=$INCLUDES
+  fi
+  grep -HIinrP $include $EXCLUDES $1 . && exit 1
+}
+
+echo "Test results:"
+
+test_command "href=.{0,1}/"
+test_command "[a-z]'s"
+test_command "\t" noinclude
+
+echo "Everything seems good..."
 exit 0
